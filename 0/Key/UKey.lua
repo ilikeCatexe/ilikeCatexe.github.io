d3446f8579a2c0e65bc836c070d3e3d06884885e
ilikeCatexe <thesanstvusep4130@gmail.com>
2024-04-22 13:16:25 +0700

    FixUKeyWorking

--- 0/Key/UKey.lua
+++ 0/Key/UKey.lua
@@ -1,1 +1,20 @@
-8sYfugrRiL0C2jQ1XkEKcUnDF
+_G.notusekeys = {
+    "kZRMdWV8br72FsNwAmjPvIaJ1",
+	"crL4U8fboT31meVZWwugOjM0H",
+	"A82abOG5Bh9ectZTindvCsw7F",
+	--"8sYfugrRiL0C2jQ1XkEKcUnDF",
+	--"AP9Gy3hQDIsfUuV45BJKFEbmg",
+	--"KFdTMwSzaNnmAh6upgfc7QiJL",
+}
+_G.keys = {
+	--"kZRMdWV8br72FsNwAmjPvIaJ1",
+	--"crL4U8fboT31meVZWwugOjM0H",
+	--"A82abOG5Bh9ectZTindvCsw7F",
+	"8sYfugrRiL0C2jQ1XkEKcUnDF",
+	"AP9Gy3hQDIsfUuV45BJKFEbmg",
+	"KFdTMwSzaNnmAh6upgfc7QiJL",
+    --"GiveFreeKey30Days",
+}
+_G.Premiumkeys = {
+	"IHATEI"
+}
