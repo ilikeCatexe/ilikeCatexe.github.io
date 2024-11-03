397f7311165b8c4862d5f3d8bae6f1993da794c7
ilikeCatexe <thesanstvusep4130@gmail.com>
2024-11-04 03:04:50 +0700

    notsup

---
+++ 0/Key/NotSupport.lua
@@ -0,0 +1,91 @@
+_G.PHmapid = {
+    3495989665, -- 1
+    2413927524, -- 2
+    3857052864, -- 3
+    8717502286, -- 4
+    10660791703, -- 5
+    9872472334, -- 6
+    6516141723, -- 7
+    4967465336, -- 8
+    8657766101, -- 9
+    7499189111, -- 10
+    6849018101, -- 11
+    9961650006, -- 12
+    7991339063, -- 13
+    11157166793, -- 14
+    166986752, -- 15
+    12605322913, -- 16
+    10912748908, -- 17
+    4390380541, -- 18
+    6766156863, -- 19
+    662417684, -- 20
+    13130031963, -- 21
+    13785756457, -- 22
+    7196856665, -- 23
+    13924946576, -- 24
+    14146061243, -- 25
+    2076502090, -- 26
+    9733390246, -- 27
+    17602539741, -- 28
+}
+
+_G.PHmapname = {
+    "The Rake Remasted", -- 1
+    "The Rake Fan Remake", -- 2
+    "King Of The Hill", -- 3
+    "Find the Pencils", -- 4
+    "cart ride around nothing", -- 5
+    "Evade", -- 6
+    "Doors", -- 7
+    "Rays Mod", -- 8
+    "Marvellous Playground", -- 9
+    "Encounters", -- 10
+    "Undertale Tower Defense", -- 11
+    "Shockwave Racing", -- 12
+    "Rainbow Friends", -- 13
+    "Ant War", -- 14
+    "Plane Crazy", -- 15
+    "zeedzad Realplayfc", -- 16
+    "Pizza Party", -- 17
+    "Rumble Quest", -- 18
+    "Strongman Simulator", -- 19
+    "LuckyBlock Battleground", -- 20
+    "Hunting Season REBIRTH", -- 21
+    "Skibiti Toilet Tower Defense", -- 22
+    "Wave sword bots game", -- 23
+    "dingus", -- 24
+    "Stand Battleground", -- 25
+    "Anime tycoon", -- 26
+    "Stair Tappers", -- 27
+    "Planet Forge", -- 28
+}
+_G.PHmapstats = {
+    "work", -- 1
+    "not work", -- 2
+    "not update", -- 3
+    "not update", -- 4
+    "work", -- 5
+    "not update", -- 6
+    "risk", -- 7
+    "work", -- 8
+    "risk", -- 9
+    "work", -- 10
+    "work", -- 11
+    "risk", -- 12
+    "not update", -- 13
+    "not update", -- 14
+    "not update", -- 15
+    "work", -- 16
+    "not update", -- 17
+    "work", -- 18
+    "work", -- 19
+    "work", -- 20
+    "not update", -- 21
+    "not update", -- 22
+    "work", -- 23
+    "work", -- 24
+    "not update", -- 25
+    "not update", -- 26
+    "not work", -- 27
+    "work", -- 28
+}
