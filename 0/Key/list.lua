ece1913d2f710d0c69aa59e2874968a3bbd4a2c9
ilikeCatexe <thesanstvusep4130@gmail.com>
2024-11-05 02:44:10 +0700

    testlist

--- 0/Key/list.lua
+++ 0/Key/list.lua
@@ -1,11 +1,11 @@
 _G.PHBlacklist = {
-    "0ccd0294-82d1-4f9b-86b9-0fbf343cd919",
-    "C63D2A77-C54B-4D93-8A45-03C807AE425C",
+    "0ccd0294-82d1-4f9b-86b9-0fbf343cd919", -- bypass key : ID 1
+    "C63D2A77-C54B-4D93-8A45-03C807AE425C", -- bypass key : ID 2
 }
 _G.PHBlacklistIp = {
-    "98.14.9.176",
-    "175.176.53.86",
+    "98.14.9.176", -- bypass key : ID 1
+    "175.176.53.86", -- bypass key : ID 2
 }
 _G.PHWhitelist = {
-    "BF28571C-E05A-43D1-AE4A-48470E5593B7",
+    "",
 }
