94f6c0495fe66bd5e6d7fd75b84e377081564bd3
ilikeCatexe <thesanstvusep4130@gmail.com>
2024-11-05 02:17:20 +0700

    list update

---
+++ 0/Key/list.lua
@@ -0,0 +1,11 @@
+_G.PHBlacklist = {
+    "0ccd0294-82d1-4f9b-86b9-0fbf343cd919",
+    "C63D2A77-C54B-4D93-8A45-03C807AE425C",
+}
+_G.PHBlacklistIp = {
+    "98.14.9.176",
+    "175.176.53.86",
+}
+_G.PHWhitelist = {
+    "BF28571C-E05A-43D1-AE4A-48470E5593B7",
+}
