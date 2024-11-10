3b54bccddfbd6955a4cf8cb352cd24e20ce08ba9
ilikeCatexe <thesanstvusep4130@gmail.com>
2024-11-10 23:01:19 +0700

    Update list.lua

--- 0/Key/list.lua
+++ 0/Key/list.lua
@@ -2,11 +2,13 @@ _G.PHBlacklist = {
     "0ccd0294-82d1-4f9b-86b9-0fbf343cd919", -- bypass key : ID 1
     "C63D2A77-C54B-4D93-8A45-03C807AE425C", -- bypass key : ID 2
     "6619ff6c-1282-4712-9251-3ad45311c069", -- bypass key : ID 3 (trying)
+    "4580f289-a277-438e-9a5b-9b2de72066d2", -- bypass key : ID 4
 }
 _G.PHBlacklistIp = {
     "98.14.9.176", -- bypass key : ID 1
     "175.176.53.86", -- bypass key : ID 2
     "161.49.222.179", -- bypass key : ID 3 (trying)
+    "124.106.65.219", -- bypass key : ID 4
 }
 _G.PHWhitelist = {
     "BF28571C-E05A-43D1-AE4A-48470E5593B7",
